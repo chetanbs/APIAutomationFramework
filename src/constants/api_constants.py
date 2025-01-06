07e48e7a67097fc8cbb0e416a5de89829b126bb5
LENOVO <chetanbsangali@gmail.com>
2025-01-06 21:56:25 +0530

    APIFramework - Implementation 2nd Jan 2025

--- src/constants/api_constants.py
+++ src/constants/api_constants.py
@@ -2,7 +2,7 @@
 # Keep the URLs
 
 
-class APIConstants:
+class APIConstants(object):
     def base_url(self):
         return "https://restful-booker.herokuapp.com"
 
@@ -12,3 +12,6 @@ class APIConstants:
     def url_create_token(self):
         return "https://restful-booker.herokuapp.com/auth"
 
+    # Booking Id -> HTTP -> Put, Patch, Delete
+    def url_patch_put_delete(booking_id):
+        return "https://restful-booker.herokuapp.com/booking"+ str(booking_id)
---
+++ src/helpers/payload_manager.py
@@ -0,0 +1,47 @@
+# Payloads
+
+# Create Booking
+# Update Booking
+# Auth - Token
+
+
+def payload_create_booking():
+    payload = {
+        "firstname": "Jim",
+        "lastname": "Brown",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    return payload
+
+
+def payload_update_booking():
+    payload = {
+        "firstname": "Jim",
+        "lastname": "Brown",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    return payload
+
+
+def payload_create_token():
+    payload = {
+        "username" : "admin123",
+        "password" : "password123"
+    }
+    return payload
+
+
+
+
